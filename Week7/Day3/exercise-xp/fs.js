037370b96d8b082c5046a31ed32d44d8313c75b8
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-05-25 17:44:21 +0300

    w7d3 xp finished

--- Week7/Day3/exercise-xp/fs.js
+++ Week7/Day3/exercise-xp/fs.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+// --- EXERCISE 1 -- //
+
 fs.readFile('./sometext.txt', 'utf-8', function (err, data) {
     if (err) {
         console.error(err)
@@ -7,9 +9,36 @@ fs.readFile('./sometext.txt', 'utf-8', function (err, data) {
     console.log(`File reads: ${data}`);
 });
 
+
+// --- EXERCISE 2 -- //
+
+
 fs.writeFile('./newdata.txt', 'New Data File', (err) => {
     if(err) {
         console.log(err)
     }
-    console.log(`New file create. Success!`)
+    console.log(`New file created. Success!`)
 })
+
+
+// --- EXERCISE 3 -- //
+
+
+fs.writeFile('./newdata2.txt', 'New Data File #2', (err) => {
+    if(err) {
+        console.log(err)
+    }
+    console.log(`New file #2 created. Success!`)
+})
+
+fs.appendFile('./newdata2.txt', '\nAppending text to New Data File #2.', function (err) {
+    if (err) throw err;
+    console.log('Updated!');
+  });
+
+  fs.unlink('./newdata2.txt', function (err) {
+    if (err) {
+        console.log(err)
+    }        
+        console.log('File deleted!');
+  });
