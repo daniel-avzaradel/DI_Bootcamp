796c1a4e658e57aff15ef203effa5ad0bf3602f5
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-05-25 22:03:44 +0300

    w7d2 xp#2.2 done xp#2.3 started

--- Week7/Day2/exercise-xp2/server.js
+++ Week7/Day2/exercise-xp2/server.js
@@ -1,6 +1,9 @@
 const exp = require('express');
 const app = exp();
 
+
+// EXERCISE 1
+
 app.use('/', exp.static(__dirname + '/public'))
 
 app.get('/user', (req, res) => {
@@ -17,3 +20,17 @@ app.set('port', 3000)
 app.listen(app.get('port'), () => {
     console.log(`App running on port ${app.get('port')}`)
 })
+
+// EXERCISE 2
+
+app.get('/:id', ((req, res) => {
+    let id = {
+        id: req.params.id
+    }
+    console.log(id)
+    res.send(JSON.stringify(id))
+}))
+
+// EXERCISE 3
+
+
