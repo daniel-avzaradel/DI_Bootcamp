56462cf77e18285f28a968fa1f0cfaca2e0edfc3
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-05-29 22:20:13 +0300

    hackathon final details

--- Week7/Day5/Hackathon2/public/js/script.js
+++ Week7/Day5/Hackathon2/public/js/script.js
@@ -4,7 +4,7 @@ const blackKeys = document.querySelectorAll('.black-keys')
 const exBtn = document.querySelectorAll('.buttons button')
 const exNote = document.getElementById('ex-note')
 const loggedUser = document.getElementById('loggedUser');
-let localUsers;
+
 
 const arrNotes = ['c', 'cs', 'd', 'ds', 'e', 'f', 'fs', 'g', 'gs', 'a', 'as', 'b',];
 
@@ -75,65 +75,67 @@ function closePop() {
 const signUp = document.getElementById('singup-form')
 const logIn = document.getElementById('login-form')
 
-let users = []
+// signUp.addEventListener('submit', e => {
+//     closePop();
 
+// })
 
-signUp.addEventListener("submit", e => {
-    e.preventDefault();
+// signUp.addEventListener("submit", e => {
+//     e.preventDefault();
 
-    let nameInp = document.querySelector('#signName')
-    let passInp = document.querySelector('#signPass')
+//     let nameInp = document.querySelector('#signName')
+//     let passInp = document.querySelector('#signPass')
 
-    console.log(nameInp.value)
-    console.log(passInp.value)
+//     console.log(nameInp.value)
+//     console.log(passInp.value)
 
-    if (localStorage.length == 0) {
+//     if (localStorage.length == 0) {
 
-        let user = {
-            name: nameInp.value,
-            password: passInp.value
-        }
-        users.push(user);
+//         let user = {
+//             name: nameInp.value,
+//             password: passInp.value
+//         }
+//         users.push(user);
     
-        localStorage.setItem('users', JSON.stringify(users))
-        console.log(JSON.parse(localStorage.getItem('users')))
-        console.log('Form submitted')
+//         localStorage.setItem('users', JSON.stringify(users))
+//         console.log(JSON.parse(localStorage.getItem('users')))
+//         console.log('Form submitted')
     
-        loggedUser.textContent = nameInp.value;
-        closePop();
+//         loggedUser.textContent = nameInp.value;
+//         closePop();
 
-    } else {
-        (JSON.parse(localStorage.getItem('users'))).find(item => {
-            if (nameInp.value == item.name && passInp.value == item.password) {
-                console.log('User already exists')
-                let dot = document.getElementById('dot')
-                dot.textContent = "User already exists"
-                dot.style.color = "salmon";
-                dot.style.visibility = "visible";
-            } 
-        })
-    }
-})
+//     } else {
+//         (JSON.parse(localStorage.getItem('users'))).find(item => {
+//             if (nameInp.value == item.name && passInp.value == item.password) {
+//                 console.log('User already exists')
+//                 let dot = document.getElementById('dot')
+//                 dot.textContent = "User already exists"
+//                 dot.style.color = "salmon";
+//                 dot.style.visibility = "visible";
+//             } 
+//         })
+//     }
+// })
 
-logIn.addEventListener("submit", e=> {
-    e.preventDefault();
+// logIn.addEventListener("submit", e=> {
+//     e.preventDefault();
 
-    let logName = document.getElementById('logName')
-    let logPass = document.getElementById('logPass')
-    let wrongInfo = document.getElementById('wrongLog')
+//     let logName = document.getElementById('logName')
+//     let logPass = document.getElementById('logPass')
+//     let wrongInfo = document.getElementById('wrongLog')
 
-    localUsers = JSON.parse(localStorage.getItem('users'))
-    console.log(localUsers)
+//     localUsers = JSON.parse(localStorage.getItem('users'))
+//     console.log(localUsers)
 
-    localUsers.find(item => {
-        if (logName.value == item.name && logPass.value == item.password) {
-            console.log('You are logged in!')
-            loggedUser.textContent = logName.value;
-            wrongInfo.style.visibility = "hidden";
-            closePop();
-            return;
-        } else {
-            wrongInfo.style.visibility = "visible";
-        }
-    })
-})
+//     localUsers.find(item => {
+//         if (logName.value == item.name && logPass.value == item.password) {
+//             console.log('You are logged in!')
+//             loggedUser.textContent = logName.value;
+//             wrongInfo.style.visibility = "hidden";
+//             closePop();
+//             return;
+//         } else {
+//             wrongInfo.style.visibility = "visible";
+//         }
+//     })
+// })
--- Week7/Day5/Hackathon2/server.js
+++ Week7/Day5/Hackathon2/server.js
@@ -1,12 +1,45 @@
 const exp = require('express');
 const app = exp();
-const fs = require('fs')
+const bp = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const urlencodedParser = bp.urlencoded({ extended: false })
+
+app.use(bp.json())
 
 app.use('/', exp.static(__dirname + '/public'))
-console.log(__dirname + '/public/')
 
 app.use(exp.json())
 
+app.post('/registered', urlencodedParser, (req, res) => {
+    try {
+        let newUser = {
+            name: req.body.name,
+            password: req.body.password
+        }
+    
+        currentUser = req.body.name;
+        console.log(`logged user is ${currentUser}`)
+    
+        fs.appendFile('users', JSON.stringify(newUser), (err) => {
+            if(err) {
+                console.log(err)
+            }
+            console.log(newUser)
+        })
+        
+        res.statusCode = 201;
+        res.sendFile(path.join(__dirname, '/public/index.html'))
+        
+
+    } catch(err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.send('Error: account not created')
+    }
+    
+})
 
 app.set('port', 3000)
 app.listen(app.get('port'), () => {
