f2592a0a1a0223238aef987ffd26fe8ab9f616de
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-05-30 18:14:00 +0300

    dc-w8d1-react-carousel npm install

--- Week8/Day1/xp-gold/xpgold/src/component/Component.js
+++ Week8/Day1/xp-gold/xpgold/src/component/Component.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 const Component = () => {
+
+    // exercise 3
+
     const mystyle = {
         color: 'white',
         backgroundColor: 'DodgerBlue',
         padding: '10px',
         fontFamily: 'Arial'
     }
+
+    // exercise 4
+
+    const mySuperStyles = {
+        color: 'yellow',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        border: '1px solid yellow',
+        backgroundColor: 'black',
+        padding: '5px',
+        borderRadius: '8px',
+        cursor: 'pointer',
+        margin: '10px'
+    }
+
+
         return (
         <div>
         <h1>This is a header</h1>
@@ -19,21 +38,25 @@ const Component = () => {
                 <input type="text"></input>
                 <button type="submit">Submit</button>
             </div>
-            <h2>Here's an image:</h2>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/819fM5kGQnL._SY355_.jpg" alt="react logo" />
-            <h2>This is a list:</h2>
+        </form>
+
+        <h2>Here's an image:</h2>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/819fM5kGQnL._SY355_.jpg" alt="react logo" />
+        <h2>This is a list:</h2>
             <ul>
                 <li>Coffee</li>
                 <li>Tea</li>
                 <li>Milk</li>
-                <h2>EXERCISE 2 : Styling React Using CSS #1</h2>
-                <header style={{color: "salmon", fontWeight: "bold"}}>Hello Style!</header>
-                <header style={{color: "salmon", fontWeight: "bold", backgroundColor: "lightblue"}}>Hello Style!</header>
-                <h2>EXERCISE 3 : Styling React Using CSS #2</h2>
-                <header style={mystyle}>Hello Style!</header>
-
             </ul>
-        </form>
+
+        <h2>EXERCISE 2 : Styling React Using CSS #1</h2>
+        <header style={{color: "salmon", fontWeight: "bold"}}>Hello Style!</header>
+        <header style={{color: "salmon", fontWeight: "bold", backgroundColor: "lightblue"}}>Hello Style!</header>
+        <h2>EXERCISE 3 : Styling React Using CSS #2</h2>
+        <header style={mystyle}>Hello Style!</header>
+        <h2>EXERCISE 4 : Styling React Using CSS #3</h2>
+        <button style={mySuperStyles}>I'm a stylish button</button>
+            
         </div>
     );
 }
