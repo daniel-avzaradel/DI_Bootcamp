ac170879e762ea8101b7fa4ee1346f1ff0b93230
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-05-30 13:58:12 +0300

    ex-xp1 done

---
+++ Week8/Day1/exercise-xp/xp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p>Exercise 1:</p>
+      <h1>HELLO WORLD!</h1>
+      <br></br>
+      <p>Exercise 2:</p>
+    </>
+  );
+}
+
+export default App;
