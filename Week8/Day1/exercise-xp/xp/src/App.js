4b9ac45ff03959ad56e429d270e8193f10fe3f8d
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-05-30 14:15:24 +0300

    ex-xp.4 done

--- Week8/Day1/exercise-xp/xp/src/App.js
+++ Week8/Day1/exercise-xp/xp/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import Myelement from './Myelement';
 
 const myelement = <h1>I Love JSX!</h1>;
+const sum = 5 + 5;
+const reactisbetter = (
+  <h1>React is {sum} times better with JSX</h1>
+)
 
 function App() {
   return (
@@ -14,6 +18,9 @@ function App() {
       <br></br>
       <p>Exercise 3:</p>
       <Myelement/>
+      <br></br>
+      <p>Exercise 4:</p>
+      {reactisbetter}
     </>
   );
 }
