45f4fa11d9531512776da51c0870d73db33bc7a7
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-05-30 19:14:08 +0300

    dc-w8d1-react-carousel

--- Week8/Day1/dc-w8d1-react-carousel/carousel/src/App.js
+++ Week8/Day1/dc-w8d1-react-carousel/carousel/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+import img1 from './assets/img1.jpg';
+import img2 from './assets/img2.webp';
+import img3 from './assets/img3.webp';
+import img4 from './assets/img4.webp';
+
+function App(props) {
+    console.log(props)
+        return (
+            <Carousel>
+                <div>
+                    <img src={img1} alt=""/>
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={img2} alt=""/>
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src={img3} alt=""/>
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <img src={img4} alt=""/>
+                    <p className="legend">Legend 4</p>
+                </div>
+            </Carousel>
+        );
+    };
 
 export default App;
