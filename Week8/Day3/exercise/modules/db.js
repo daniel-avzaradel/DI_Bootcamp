8d97257645ff38be6e680ffd971be8edfbbf48b0
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-01 12:40:21 +0300

    created db.js file

---
+++ Week8/Day3/exercise/modules/db.js
@@ -0,0 +1,12 @@
+const knex = require('knex');
+
+const db = knex({
+    client: pg,
+    connection: {
+        host: '127.0.0.1',
+        port: '5555',
+        user: 'postgres',
+        password: 'daniel88',
+        database: 'dvdrental'
+    }
+})
