e5495be5526d29a156aea7eedb6e37f58cb8fc98
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-02 10:27:15 +0300

    w8d4 start

---
+++ Week8/Day4/exercises/w8d4-exercises/src/components/Dropdown.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Dropdown extends Component {
+    constructor(){
+        super();
+        this.state ={
+            show: true
+        }
+    }
+
+    render() {
+        return(
+            <h1>Hello</h1>
+        )
+    }
+}
