645f61a4b317c5bd236fcec207f3c6bd2d4455c3
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-03 03:47:26 +0300

    updating components for xp-errorboundary

--- Week8/Day4/exercises/xp/xp/src/BuggyCounter.js
+++ Week8/Day4/exercises/xp/xp/src/BuggyCounter.js
@@ -1,24 +1,97 @@
 import React from 'react';
 
-class BuggyCounter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+    class ErrorBoundary extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                error: false,
+                errorInfo: false
+            }
+        }
 
+        componentDidCatch(error, errorInfo) {
+            this.setState({
+                error: error, 
+                errorInfo: errorInfo
+            });
+        }
+
+        render() {
+            if(this.state.errorInfo) {
+                return(
+                    <div>
+                    <h1>Something went wrong.</h1>
+                    <details style={{ whiteSpace: 'normal'}}>
+                        <sumarry>Details</sumarry>
+                        {this.state.error && this.state.error.toString()}
+                        <br/>
+                        {this.state.errorInfo.componentStack}      
+                    </details>
+                    </div>
+                )
+            }
+            return this.props.children;
         }
     }
 
-    render() {
+
+
+    class BuggyCounter extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {counter: 0};
+            this.handleClick = this.handleClick.bind(this);
+        }
+
+        handleClick(){
+            this.setState(({counter}) => ({
+                counter: counter + 1
+            }));
+        }
+
+        render(){
+                if(this.state.counter === 5) {
+                    throw new Error('Cashed Application')
+                }
+                return (
+                    <h1 onClick={this.handleClick()}>{this.state.counter}</h1>
+                )
+        }
+   }
+
+    function App() {
         return(
             <div>
-            <strong>
-            <p>Click on the numbers to increase the counters.</p>
-            <p>The counter is programmed to throw error when it reaches 5. This simulates a JavaScript error in a component.</p>
-            </strong>
-            <hr></hr>
-            <h1>These two counters are inside the same error boundary. If one crashes, the error boundary will replace both of them.</h1>
-            <details>Details</details>
+                {/* header of the page */}
+                <strong>
+                <p>Click on the numbers to increase the counters.</p>
+                <p>The counter is programmed to throw error when it reaches 5. This simulates a JavaScript error in a component.</p>
+                </strong>
+                <hr></hr>
+                {/* Simulation 1 */}
+                <ErrorBoundary>
+                    <p>These two counters are inside the same error boundary. If one crashes, the error boundary will replace both of them.</p>
+                    <BuggyCounter /><br/>
+                    <BuggyCounter /><br/>
+                </ErrorBoundary>
+                <hr/>
+                {/* Simulation 2 */}
+                <ErrorBoundary>
+                    <p>These two counters are each inside of their own error boundary. So if one crashes, the other is not affected.</p>
+                    <BuggyCounter />
+                <ErrorBoundary>
+                <br/>
+                </ErrorBoundary>
+                    <BuggyCounter />
+                </ErrorBoundary>
+                <br/>
+                <hr/>
+                {/* Simulation 3 */}
+                    <p>This counter is not inside of boundary. So if crashes, all other components are deleted.</p>
+                    <BuggyCounter />
+                <br/>
             </div>
         )
     }
-}
+
+export default App;
