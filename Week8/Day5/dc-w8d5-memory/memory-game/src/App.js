0b2cc7f48c75dbc8aac06f0ca12daa3221ba26f2
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-06 12:00:31 +0300

    finished dc-w8d5

--- Week8/Day5/dc-w8d5-memory/memory-game/src/App.js
+++ Week8/Day5/dc-w8d5-memory/memory-game/src/App.js
@@ -1,72 +1,82 @@
+// get the json into state array
+// shuffle function
+// score - state
+// top_score - state
+
 import React from 'react';
-import superheroes from './superheroes.json';
-import Hero from './components/Hero';
+import Hero from './components/Hero'
 import Nav from './components/Nav'
+import './components/hero.css'
+import './App.css';
+import superheroes from './superheroes.json';
 
 class App extends React.Component {
-  constructor() {
+  constructor(){
     super();
-    console.log(superheroes)
-
     this.state = {
-        heroes: [],
-        score: 0,
-        top_score: 0
+      heros: [],
+      score: 0,
+      top_score: 0
     }
   }
-  componentDidMount() {
-      this.setState({heroes: superheroes.superheroes})
+  componentDidMount(){
+    this.setState({heros:superheroes.superheroes})
   }
-
   handleClick = (id) => {
-    let {heroes, score, top_score} = this.state;
-    const hero = heroes.find(item => {
-      return item.id === id;
+    let {heros,score,top_score} = this.state;
+    const hero = heros.find(item => {
+      return item.id === id
     })
-    if(hero.clicked === true) {
-      // reset the game
-      if(score > top_score) {
-          this.setState({
-            top_score: score
-          })
+    if(hero.clicked === true){
+      //reset the game
+      if(score>top_score){
+        this.setState({
+          top_score:score
+        })
       }
       this.setState({
-        score: 0
+        score:0,
+        heros: this.reset([...heros])
       })
-
-    } else {
+    }
+    else{
       hero.clicked = true;
       this.setState({
-        score: score++,
-        heroes: this.shuffle([...heroes])
+        score:++score,
+        heros: this.shuffle([...heros])
       })
     }
   }
-
-  shuffle = (array) => {
-    for(let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i+1));
+  reset = (array) => {
+    for(let i = array.length-1; i>0; i--){
+      array[i].clicked = false
+    }
+    return array
+  }
+  shuffle = (array) =>{
+    for (var i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
       // let t = array[i];
       // array[i] = array[j];
       // array[j] = t;
-      [array[i], array[j]] = [array[j], array[i]]
+      [array[i],array[j]] = [array[j],array[i]]
     }
+    return array;
   }
-
   render() {
-    const { heroes, score, top_score } = this.state
-    return (
+    const {heros,score,top_score} = this.state;
+    return(
       <>
-      <Nav {...this.state} />
-      <div className='herolist'>
-        {
-          heroes.map((item, i) => {
-            return <Hero handleClick={this.handleClick} hero={item} key={i}/>
-          })
-        }
-      </div>
+        <Nav {...this.state} />
+        <div className='heroslist'>
+          {
+            heros.map((item,i)=>{
+              return <Hero hero={item} handleClick={this.handleClick} key={i} />
+            })
+          }
+        </div>
       </>
-    );
+      )
   }
 }
 
--- Week8/Day5/dc-w8d5-memory/memory-game/src/components/Nav.js
+++ Week8/Day5/dc-w8d5-memory/memory-game/src/components/Nav.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-const Nav = (props) => {
-    const {score, top_score} = props;
-    return(
-        <div>
-            <h2>Score: {score}<br/>
-            Top Score: {top_score}</h2>
-        </div>
-    )
+const Nav = ({score, top_score}) => {
+  // const {score, top_score} = props;
+  return(
+      <div>
+        <h2>Super Heroes Memory Game</h2>
+        Score: {score}  Top Score: {top_score}
+      </div>
+  )
 }
-
-export default Nav;
+export default Nav
