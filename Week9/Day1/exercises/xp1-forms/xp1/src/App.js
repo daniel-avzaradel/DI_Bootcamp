087ffa4f7c0bd56951e70dd3754c2ccd7ec3efcd
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-07 00:10:50 +0300

    form submission complete

--- Week9/Day1/exercises/xp1-forms/xp1/src/App.js
+++ Week9/Day1/exercises/xp1-forms/xp1/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      author: '',
+      genre: '',
+      year: ''
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmitForm(event) {
+    event.preventDefault();
+    console.log(this.state)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1>NEW BOOK</h1>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Title" name="title" value={this.state.title} onChange={this.onInputChange} />
+          <input type="text" placeholder="Author" name="author" value={this.state.author} onChange={this.onInputChange} />
+          <input type="text" placeholder="Genre" name="genre" value={this.state.genre} onChange={this.onInputChange} />
+          <input type="number" placeholder="Year Published" name="year" value={this.state.year} onChange={this.onInputChange} />
+          <br/>
+          <button type="submit" value="Submit" onClick={this.onSubmitForm}>Submit</button>
+          <p style={{color: 'green'}}>Form sent successfully!</p>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
