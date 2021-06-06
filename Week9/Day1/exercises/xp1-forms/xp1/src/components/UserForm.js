a1f7be8ced16880e75ccdefe577c7db4c98c059c
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-07 00:22:58 +0300

    preparing ex-xp#1.2 - user input form

---
+++ Week9/Day1/exercises/xp1-forms/xp1/src/components/UserForm.js
@@ -0,0 +1,50 @@
+import './UserForm.css';
+import React from 'react'
+
+class UserForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      author: '',
+      genre: '',
+      year: ''
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmitForm(event) {
+    event.preventDefault();
+    console.log(this.state)
+    document.getElementById('success').style.display = 'block';
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1 id="welcome">WELCOME!</h1>
+        <p style={{padding: '0 6px'}}>Please provide your information bellow.</p>
+        <br/>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Title" name="title" value={this.state.title} onChange={this.onInputChange} />
+          <input type="text" placeholder="Author" name="author" value={this.state.author} onChange={this.onInputChange} />
+          <input type="text" placeholder="Genre" name="genre" value={this.state.genre} onChange={this.onInputChange} />
+          <input type="number" placeholder="Year Published" name="year" value={this.state.year} onChange={this.onInputChange} />
+          <br/>
+          <button type="submit" value="Submit" onClick={this.onSubmitForm}>Submit</button>
+          <p id="success" style={{color: 'green'}}>Form sent successfully!</p>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UserForm;
--- Week9/Day1/exercises/xp1-forms/xp1/src/index.js
+++ Week9/Day1/exercises/xp1-forms/xp1/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import UserForm from './components/UserForm';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <br/>
+    <UserForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
