c5ce062d13ffd52a75a1dba056e4c15184746bd8
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-07 03:56:12 +0300

    installed react-router-dom

--- Week9/Day1/exercises/xp2-router/xp2-router-error/src/App.js
+++ Week9/Day1/exercises/xp2-router/xp2-router-error/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './components/Home'
+import Profile from './components/Profile'
+import Shop from './components/Shop'
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+          <NavBar/>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/profile' component={Profile}/>
+            <Route path='/shop' component={Shop}/>
+          </Switch>
+      </div>
+      );
+  }
 }
 
 export default App;
