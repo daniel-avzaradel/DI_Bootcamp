b7a882a66cab62fdcf6f591b8214ae9cb4721228
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-09 21:59:00 +0300

    finished counter with pure react. starting with redux

--- Week9/Day2/exercises/xp-counter-redux/src/App.js
+++ Week9/Day2/exercises/xp-counter-redux/src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div>
-      <h1>HELLO REACT</h1>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  decrease = () => {
+    const {counter} = this.state;
+    this.setState({
+      counter: counter - 1
+    })
+  }
+
+  increase = () => {
+    const {counter} = this.state;
+    this.setState({
+      counter: counter + 1
+    })
+  }
+
+  render() {
+
+    const {counter} = this.state;
+
+    return (
+      <div className="counter">
+        <h1>REACT COUNTER</h1>
+        <p>by: Daniel avzaradel</p>
+        <br/>
+        <div className="buttons">
+          <button onClick={this.decrease}>-</button>{counter}<button onClick={this.increase}>+</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
