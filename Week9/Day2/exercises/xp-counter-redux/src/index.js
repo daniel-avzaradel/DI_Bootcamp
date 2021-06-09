652ceaaba780edcf619dab64248744b9bc85ff28
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-10 01:10:39 +0300

    working on redux

--- Week9/Day2/exercises/xp-counter-redux/src/index.js
+++ Week9/Day2/exercises/xp-counter-redux/src/index.js
@@ -4,6 +4,48 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION INCREMENT
+
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+// ACTION DECREASE
+
+const decrease = () => {
+  return {
+    type: 'DECREASE'
+  }
+}
+
+// REDUCER
+
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+        return state + 1;
+        break;
+    case 'DECREASE':
+        return state - 1;
+        break;
+  }
+}
+
+const store = createStore(counter)
+
+// display in the console
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
