16fd538526213d400afbc2f6a0ff4c44c79bc838
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-10 09:49:13 +0300

    started movielist redux project

---
+++ Week9/Day2/exercises/dc-w9d2-movies/src/components/MovieDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieDetails = (props) => {
+    return (
+        <div>
+            <h1>Movie Details</h1>
+        </div>
+    )
+}
+
+export default MovieDetails;
--- Week9/Day2/exercises/dc-w9d2-movies/src/components/MovieList.js
+++ Week9/Day2/exercises/dc-w9d2-movies/src/components/MovieList.js
@@ -1,1 +1,19 @@
-import {moviesReducer} from '../reducers'
+import React from 'react';
+
+class MovieList extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                <h1>Movie List</h1>
+                </div>
+                <div>
+                    <h3>Movie Title</h3>
+                    <button>Detail</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MovieList;
