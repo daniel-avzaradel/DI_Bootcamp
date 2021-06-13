d414b331598dac1fffc4a42b51f7be5d0c9f9dd7
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-13 10:30:51 +0300

    useState and useEffect

--- Week9/Day4/exercises/ex/src/App.js
+++ Week9/Day4/exercises/ex/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const [state, setState] = useState(0)
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState('');
+
+  return(
+    <div className="App-header">
+        <h1>HOOKS</h1>
+        <h2>{counter}</h2>
+        <button onClick={() => setCounter(counter+1)}>Add</button>
+        <h2>My name is: {name}</h2>
+        <input type="text" onChange={(event) => setName(event.target.value)} />
+      </div>
+  )
 }
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       counter: 0
+//     }
+//   }
+
+//   add = () => {
+//     this.setState({counter: this.state.counter++})
+//   }
+
+//   render() {
+//     const {counter} = this.state;
+//     return (
+//       <div className="App">
+//         <h1>HOOKS</h1>
+//         <h2>{this.state.counter}</h2>
+//         <button onClick={this.add}>Add</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
