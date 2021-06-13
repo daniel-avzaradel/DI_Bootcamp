e14a7843ffd06d06aaa18a8e39761d5e0949e853
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-13 23:29:56 +0300

    imported Link, NavLink from react-router-dom to apply in the Navbar

--- Week9/Day5/Mini-Project/redux-blog/src/App.js
+++ Week9/Day5/Mini-Project/redux-blog/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
 
 class App extends React.Component {
 
-
   render() {
     return (
-      <div className="App">
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar/>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- Week9/Day5/Mini-Project/redux-blog/src/components/Contact.js
+++ Week9/Day5/Mini-Project/redux-blog/src/components/Contact.js
@@ -4,7 +4,8 @@ const Contact = () => {
     return(
         <div className="container">
             <h4 className="center">Contact</h4>
-            <p>Cillum est eu aliqua ullamco consequat Lorem officia nulla dolore ut duis reprehenderit fugiat deserunt. Ut aute laboris fugiat velit. Non labore cillum voluptate occaecat sunt adipisicing incididunt Lorem cupidatat.
+            <p>Cillum est eu aliqua ullamco consequat Lorem officia nulla dolore ut duis reprehenderit fugiat deserunt. Ut aute laboris fugiat velit. Non labore cillum voluptate occaecat sunt adipisicing incididunt Lorem cupidatat.<br/>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit nemo consectetur vitae nam? Molestiae nulla consectetur maxime. Nostrum optio tenetur libero adipisci ipsum, iusto quibusdam ex vitae aperiam. Nobis, eum.
             </p>
         </div>
     )
