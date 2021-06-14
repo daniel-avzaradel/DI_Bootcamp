59d1b1603dd3503aae899822d7f1290b05d0d8ef
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-14 23:23:36 +0300

    password bcrypt login

--- Week7/exercises/Ex1/server.js
+++ Week7/exercises/Ex1/server.js
@@ -1,6 +1,11 @@
 const exp = require('express');
 const app = exp();
 const bcrypt = require('bcrypt')
+const initializePassport = require('./passport-config')
+
+initialize(passport, email => {
+    users.find(user => user.email === email)
+})
 
 const users = [];
 
@@ -22,10 +27,10 @@ app.post('/register', async (req, res) => {
     try{
         const hashPassword = await bcrypt.hash(req.body.password, 10)
         users.push({
-            id: Date.now.toString(),
+            id: Date.now().toString(),
             name: req.body.name,
             email: req.body.email,
-            password: hashedPassword
+            password: hashPassword
         })
         res.redirect('/login')
     } catch {
