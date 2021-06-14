b1debbf10745fb02709262cd9944679bb170eccc
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-15 01:35:35 +0300

    new todo list react app

---
+++ Week8/exercises/todolist-react/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+import Form from './components/Form'
+import TodoList from './components/TodoList'
+
+function App() {
+  const [inputText, setInputText] = useState('');
+
+  return (
+    <div className="App">
+      <header>
+        <h1>REACT TODO-LIST</h1>
+      </header>
+      <Form setInputText={setInputText} />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
