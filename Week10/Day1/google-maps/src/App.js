4e6e76d59ecacbb9424aa695922584e4a349e7d0
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-17 00:27:23 +0300

    starting final project - pokedex

--- Week10/Day1/google-maps/src/App.js
+++ Week10/Day1/google-maps/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleMap from './components/GoogleMap'
 
 function App() {
   return (
     <div className="App">
       <h1>GOOGLE MAPS API with REACT JS</h1>
       <br />
-      
+      <GoogleMap />
     </div>
   );
 }
