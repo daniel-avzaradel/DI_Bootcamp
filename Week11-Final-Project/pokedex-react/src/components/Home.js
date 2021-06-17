22c7a3155e368abe5fd3548a1766468c86a20e68
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-17 16:15:17 +0300

    pokedex react

--- Week11-Final-Project/pokedex-react/src/components/Home.js
+++ Week11-Final-Project/pokedex-react/src/components/Home.js
@@ -1,8 +1,56 @@
 import React from 'react';
 
-const Home = () => {
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            url: [],
+            pokemon: []
+        }
+    }
+
+    componentDidMount() {
+
+        fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                pokemon: data.results,
+            })
+            for(let i=1; i<=151; i++) {
+                fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+                .then(res => res.json())
+                .then(data => {
+                    this.setState({
+                        url: data
+                    })
+                })
+            }
+        })
+    }
+
+    render() {
+        const {pokemon} = this.state;
+        return(
+            <>
+              {
+                  pokemon.map((item, i) => {
+                      return (
+                        <Card key={i} pokemon={item} />
+                      )
+                  })
+              }
+            </>
+        )
+    }
+}
+
+const Card = ({pokemon}) => {
     return(
-        <h1>HOME</h1>
+        <div className="p-2 m-2 card bg-light darken-3">
+            {pokemon.name}
+        </div>
     )
 }
 
