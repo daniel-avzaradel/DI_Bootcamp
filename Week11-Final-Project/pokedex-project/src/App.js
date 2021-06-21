e14ac0787b7c36cd54a693ca25c4d8262d6f9784
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-21 17:17:22 +0300

    fetching the api

--- Week11-Final-Project/pokedex-project/src/App.js
+++ Week11-Final-Project/pokedex-project/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Myteams from './components/Myteams';
 import Pokedex from './components/Pokedex'
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([])
+  const url = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch(url)
+      let data = await response.json();
+      setPokemonData(data.results)
+      console.log(pokemonData);
+    }
+    fetchData();
+  }, [])
+
   return (
     <div>
       <BrowserRouter>
