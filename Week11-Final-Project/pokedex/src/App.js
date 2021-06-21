fe56563d332242876b35d6365aecc0ff618304f3
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-21 11:21:35 +0300

    new final project

---
+++ Week11-Final-Project/pokedex/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { getAllPokemon, getPokemon } from './services/pokemon';
+import Card from './components/Cards/Card'
+import './App.css';
+
+function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl)
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      let loading = await loadingPokemon(response.results);
+
+      setLoading(false)
+    }
+    fetchData();
+  }, [])
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord;
+    }))
+
+    setPokemonData(_pokemonData);
+  }
+  console.log(pokemonData);
+  return (
+    <div>
+      {
+        loading ? <h1>Loading...</h1> : (
+          <>
+            <div className="grid-container">
+              {
+                pokemonData.map((pokemon, i) => {
+                  return <Card pokemon={pokemon} key={i} />
+                })
+              }
+            </div>
+          </>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
