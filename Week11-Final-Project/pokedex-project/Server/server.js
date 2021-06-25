1ec489b65102a23c478e1337f9d4483b70cc15bf
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-06-26 00:30:58 +0300

    connecting database

--- Week11-Final-Project/pokedex-project/Server/server.js
+++ Week11-Final-Project/pokedex-project/Server/server.js
@@ -1,13 +1,29 @@
 const exp = require('express');
 const app = exp();
 
+let db = require('knex')({
+    client: 'pg',
+    version: '7.2',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'daniel88',
+      database : 'pokedex',
+      port: 5432
+    }
+});
+
+app.set("db", db);
+
 const bp = require('body-parser')
 
 app.use(bp.urlencoded({ extended: false }))
 app.use(bp.json())
 
 app.get('/login', (req, res) => {
-    res.send({message: 'ok'})
+    db
+    .select('*').from('users')
+    .then(user => console.log(user));
 })
 
 app.listen(5000, () => {
